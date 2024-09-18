40da127fa7beae9f2777af4b1754676573450f18
MACBOOK <noorul.haque@gmail.com>
2024-09-18 17:00:57 +0530

    update

---
+++ tf-test.groovy
@@ -0,0 +1,23 @@
+pipeline {
+    agent any
+
+    stages {
+        stage('checkout') {
+            steps {
+                checkout scmGit(branches: [[name: '*/main']], extensions: [], userRemoteConfigs: [[url: 'https://github.com/noorul-haque/gcp-learning.git']])
+            }
+        }
+
+        stage('workspace') {
+            steps {
+                sh '''
+                  set -x
+                  gcloud auth activate-service-account  ansible@gcp-learning-428307.iam.gserviceaccount.com --key-file=/var/lib/jenkins/gke.json
+                  cd /var/lib/jenkins/workspace/git-checkout-pipeline/
+                  terraform init
+                '''
+            }
+        }      
+        
+    }
+}
